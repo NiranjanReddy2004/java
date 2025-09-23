7f31bd5bb8e6003c25f0344c063e9682f6ba31c8
NiranjanReddy2004 <kniranjanreddy2004@gmail.com>
2025-09-23 21:50:56 +0530

    sort

---
+++ Array/demo.java
@@ -0,0 +1,15 @@
+package Array;
+
+import java.util.Arrays;
+
+public class demo {
+    public static void main(String[] args) {
+        int[]a= {1,2,3};
+        m1(a);
+        System.out.println(Arrays.toString(a));
+    }
+    public static void m1(int[]a){
+        a[0]++;
+    }
+    
+}
